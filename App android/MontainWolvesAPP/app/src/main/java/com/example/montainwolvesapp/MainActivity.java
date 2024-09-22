ce0f240dc6e1e738f2eaca71cef84c514df41246
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2024-09-22 18:57:52 +0100

    Menu update, sound intro added

--- App android/MontainWolvesAPP/app/src/main/java/com/example/montainwolvesapp/MainActivity.java
+++ App android/MontainWolvesAPP/app/src/main/java/com/example/montainwolvesapp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.montainwolvesapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,11 +20,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton playPause;
+    ImageView teamLogo;
+    MediaPlayer mediaPlayer;
+    boolean sound = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer=MediaPlayer.create(this,R.raw.intro);
+        mediaPlayer.start();
+        playPause = (ImageButton) findViewById(R.id.play_pause_intro);
+
+
+        playPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                introSound();
+            }
+        });
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,27 +57,51 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.home){
             Toast.makeText(this,item + " Selected!", Toast.LENGTH_SHORT).show();
+            home();
             return true;
         }else if (item.getItemId() == R.id.settings) {
+            Toast.makeText(this, item + " Selected!", Toast.LENGTH_SHORT).show();
+            settings();
+            return true;
+        }else if(item.getItemId() == R.id.team){
             Toast.makeText(this,item + " Selected!", Toast.LENGTH_SHORT).show();
+            team();
             return true;
         } else if (item.getItemId() == R.id.contact_us) {
             Toast.makeText(this,item + " Selected!", Toast.LENGTH_SHORT).show();
+            contactUs();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    public void introSound(){
+        if(sound){
+            sound = false;
+        }else{
+            sound = true;
+        }
+
+        if(sound) {
+            mediaPlayer.start();
+        }else{
+            mediaPlayer.pause();
+        }
+    }
 
     public void home(){
-
+        setContentView(R.layout.activity_main);
     }
 
     public void settings(){
-
+        setContentView(R.layout.activity_settings);
     }
-    
-    public void contactUs(){
 
+    public void team(){
+        setContentView(R.layout.team);
+    }
+
+    public void contactUs(){
+        setContentView(R.layout.activity_contact_us);
     }
 }
