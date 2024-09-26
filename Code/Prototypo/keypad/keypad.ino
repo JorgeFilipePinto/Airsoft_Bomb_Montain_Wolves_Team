14bda618557f404897020c8cb6eb1d1a9a409259
JorgeFilipePinto <jorge.filipepinto@hotmail.com>
2024-09-26 21:45:25 +0100

    keypad with LCD testes and millis function

---
+++ Code/Prototypo/keypad/keypad.ino
@@ -0,0 +1,182 @@
+#include <LCD-I2C.h>
+#include <Wire.h>
+#include <Keypad.h>
+
+#define BUZZER 0
+
+void funcTone(int);
+
+unsigned long oldTone = millis();
+unsigned long newTone;
+
+String code = "";
+String secondCode = "";
+int codeSize = 0;
+enum CodeStatus {
+  InsertCode,
+  TryCode,
+  VerifyCode};
+  
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {13, 12, 14,27}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {26, 25, 33, 32}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+LCD_I2C lcd(0x27, 16, 2);
+CodeStatus codeStatus = InsertCode;
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("Montain Wolves");
+  Serial.println("Airsoft Bomb");
+  pinMode(BUZZER, OUTPUT);
+  Wire.begin();
+  lcd.begin(&Wire);
+  lcd.display();
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  lcd.print("Montain Wolves");
+  lcd.setCursor(2,1);
+  lcd.print("Airsoft Bomb");
+  char key;
+  while(key != 'D'){
+    key = keypad.getKey();
+    lcd.setCursor(9,0);
+    lcd.print("Montain Wolves");
+    lcd.setCursor(5,1);
+    lcd.print(F("Press D to continue!!"));
+    for(int i = 0; i < 10; i++){
+      key = keypad.getKey();
+      if(key == 'D'){
+        break;
+      }
+      lcd.scrollDisplayLeft();
+      delay(300);
+    }
+    lcd.clear();
+  }
+}
+
+void loop(){
+  switch(codeStatus){
+    case InsertCode: {
+      lcd.setCursor(0,0);
+      lcd.print("Insert Bomb Code");
+      char key = keypad.getKey();
+      if(key){
+        tone(BUZZER, 1000);
+        delay(100);
+        noTone(BUZZER);
+        if(key == 'D'){
+          Serial.println("Codigo inserido com sucesso!");
+          Serial.print("code size ");
+          Serial.println(codeSize);
+          codeStatus = TryCode;
+          oldTone = millis();
+          noTone(BUZZER);
+          lcd.clear();
+        }else if (key == 'C' & codeSize > 0){
+          lcd.clear();
+          codeSize--;
+          String tempCode = code.substring(0,code.length()-1);
+          code = tempCode;
+          lcd.setCursor(4,1);
+          lcd.print(code);
+        }else{
+          lcd.setCursor(codeSize + 4, 1);
+          code += key;
+          codeSize++;
+          Serial.print("Digit Insert ");
+          Serial.println(key);
+          lcd.print(key);
+        }
+      }
+      break;
+    }
+    case TryCode: {
+      lcd.setCursor(2,0);
+      lcd.print("Bomb Armed!");
+      char key = keypad.getKey();
+      funcTone(1000);
+      if(key){
+        noTone(BUZZER);
+        tone(BUZZER, 500);
+        delay(100);
+        noTone(BUZZER);
+        if(codeSize > 0){
+          secondCode += key;
+          Serial.print("Digit Insert ");
+          Serial.println(key);
+          codeSize--;
+          Serial.print("Faltam ");
+          Serial.println(codeSize);
+        }
+      }
+      if(codeSize == 0){
+        codeStatus = VerifyCode;
+        lcd.clear();
+        }
+      break;
+    }
+    case VerifyCode: {
+      if(code == secondCode){
+        lcd.setCursor(1,0);
+        lcd.print("Correct Code!");
+        lcd.setCursor(4,1);
+        lcd.print("YOU WIN!");
+        Serial.println("correct code!");
+        delay(5000);
+        lcd.clear();
+        codeStatus = InsertCode;
+      }else{
+        lcd.setCursor(3,0);
+        lcd.print("Try Again!!");
+        Serial.println("Try Again!!");
+        codeSize = code.length();
+        codeStatus = TryCode;
+        secondCode = "";
+        delay(5000);
+        lcd.clear();
+      }
+      break;
+    }
+  }
+
+  /*
+  char key = keypad.getKey();
+  if(key == 'D'){
+    for(char digit : code){
+      Serial.println(digit);
+    }
+  }else if (key){
+    Serial.println(key);
+    code += key;
+    Serial.println(code);
+  }*/
+}
+
+
+void funcTone(int time){
+  newTone = millis();
+  if(newTone - oldTone < 2000){
+    if(newTone - oldTone < time){
+      tone(BUZZER, 1500);
+    }else{
+      noTone(BUZZER);
+    }
+  }else{
+    oldTone = millis();
+  }
+}
+
+
+
+
--- Testes/keypad/keypad.ino
+++ Testes/keypad/keypad.ino
@@ -1,14 +1,21 @@
-/* @file HelloKeypad.pde
-|| @version 1.0
-|| @author Alexander Brevig
-|| @contact alexanderbrevig@gmail.com
-||
-|| @description
-|| | Demonstrates the simplest use of the matrix Keypad library.
-|| #
-*/
+
 #include <Keypad.h>
+
+#define BUZZER 0
+
+void funcTone(int);
+
+unsigned long oldTone = millis();
+unsigned long newTone;
+
 String code = "";
+String secondCode = "";
+int codeSize = 0;
+enum CodeStatus {
+  InsertCode,
+  TryCode,
+  VerifyCode};
+  
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
@@ -22,12 +29,74 @@ byte rowPins[ROWS] = {13, 12, 14,27}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {26, 25, 33, 32}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+CodeStatus codeStatus = InsertCode;
 
 void setup(){
   Serial.begin(115200);
+  pinMode(BUZZER, OUTPUT);
 }
-  
+
 void loop(){
+  switch(codeStatus){
+    case InsertCode: {
+      char key = keypad.getKey();
+      if(key){
+        tone(BUZZER, 1000);
+        delay(100);
+        noTone(BUZZER);
+        if(key == 'D'){
+          Serial.println("Codigo inserido com sucesso!");
+          Serial.print("code size ");
+          Serial.println(codeSize);
+          codeStatus = TryCode;
+          oldTone = millis();
+          noTone(BUZZER);
+        }else{
+          code += key;
+          codeSize++;
+          Serial.print("Digit Insert ");
+          Serial.println(key);
+        }
+      }
+      break;
+    }
+    case TryCode: {
+      char key = keypad.getKey();
+      funcTone(1000);
+      if(key){
+        noTone(BUZZER);
+        tone(BUZZER, 500);
+        delay(100);
+        noTone(BUZZER);
+        if(codeSize > 0){
+          secondCode += key;
+          Serial.print("Digit Insert ");
+          Serial.println(key);
+          codeSize--;
+          Serial.print("Faltam ");
+          Serial.println(codeSize);
+        }
+      }
+      if(codeSize == 0){
+        codeStatus = VerifyCode;
+        }
+      break;
+    }
+    case VerifyCode: {
+      if(code == secondCode){
+        Serial.println("correct code!");
+        codeStatus = InsertCode;
+      }else{
+        Serial.println("Try Again!!");
+        codeSize = code.length();
+        codeStatus = TryCode;
+        secondCode = "";
+      }
+      break;
+    }
+  }
+
+  /*
   char key = keypad.getKey();
   if(key == 'D'){
     for(char digit : code){
@@ -37,5 +106,23 @@ void loop(){
     Serial.println(key);
     code += key;
     Serial.println(code);
+  }*/
+}
+
+
+void funcTone(int time){
+  newTone = millis();
+  if(newTone - oldTone < 2000){
+    if(newTone - oldTone < time){
+      tone(BUZZER, 1500);
+    }else{
+      noTone(BUZZER);
+    }
+  }else{
+    oldTone = millis();
   }
 }
+
+
+
+
